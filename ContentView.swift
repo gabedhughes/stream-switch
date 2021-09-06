8cae253fb77c0c211b1fccfc43cb32b539621143
Gabe Hughes <gdhughes5@gmail.com>
2021-09-06 12:58:04 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StreamSwitch
+//
+//  Created by Gabe Hughes on 10/12/19.
+//  Copyright © 2019 Gabe Hughes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
